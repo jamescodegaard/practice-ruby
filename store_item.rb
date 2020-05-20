74b63244f6c53725f9102af8866f75868d48326b
jamescodegaard <jamescodegaard@gmail.com>
2020-05-20 14:01:23 -0500

    created item class

---
+++ store_item.rb
@@ -0,0 +1,37 @@
+# item1 = {name: "Nag Champa", type: "scent", price: 4.99}
+# item2 = {name: "Marjoram", type: "herb", price: 3.50}
+# item3 = {name: "Grape Jelly", type: "spread", price: 7.99}
+
+class Item 
+
+  def initialize(input_name, input_type, input_price)
+    @name = input_name
+    @type = input_type
+    @price = input_price
+  end
+
+  def name
+    @name
+  end
+
+  def type
+    @type
+  end
+
+  def price
+    @price
+  end
+
+end
+
+item1 = Item.new("Nag Champa", "Scent", 4.99)
+item2 = Item.new("Marjoram", "herb", 3.50)
+item3 = Item.new("Grape Jelly", "spread", 7.99)
+
+puts "Item: #{item1.name}"
+puts "Type: #{item1.type}"
+puts "Price: #{item1.price}"
+puts
+puts "#{item2.name} costs #{item2.price}."
+
+
